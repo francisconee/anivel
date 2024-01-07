79442510bf49673d482ae647e9ad31e4cee7d0bd
francisconee <francisco.loy@gmail.com>
2024-01-07 17:01:41 -0300

    avances generales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,52 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { WorkDetailComponent } from './pages/work-detail/work-detail.component';
+import { RegisterComponent } from './pages/admin/components/register/register.component';
+import { LoginComponent } from './pages/admin/components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: MainComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'admin/add-work',
+    component: AdminComponent,
+    pathMatch: 'full'
+    
+  },
+  {
+    path: 'admin/works',
+    component: AdminComponent,
+    pathMatch: 'full'
+    
+  },
+  { path: 'detalle/:id',
+    component: WorkDetailComponent,
+    pathMatch: 'full'
+  },
+  { path: 'registrarse',
+    component: RegisterComponent,
+    pathMatch: 'full'
+  },
+  { path: 'login',
+    component: LoginComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,62 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { FormCotizarComponent } from './components/form-cotizar/form-cotizar.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ListWorksComponent } from './components/list-works/list-works.component';
+import { CardWorkComponent } from './components/card-work/card-work.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ValoresComponent } from './components/valores/valores.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { AdminModule } from './pages/admin/admin.module';
+import { WorkDetailComponent } from './pages/work-detail/work-detail.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContactComponent,
+    HeroComponent,
+    FormCotizarComponent,
+    ListWorksComponent,
+    CardWorkComponent,
+    AboutusComponent,
+    ValoresComponent,
+    FooterComponent,
+    AdminComponent,
+    WorkDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    AdminModule,
+    provideFirebaseApp(() => initializeApp({
+      "projectId": "pinta-gonzalo",
+      "appId": "1:654289781621:web:bebc6cddce6e6dd1dc493c",
+      "storageBucket": "pinta-gonzalo.appspot.com",
+      "apiKey": "AIzaSyAvCUwO3oqiauHRY-0y99E0u7uT9o8q08c",
+      "authDomain": "pinta-gonzalo.firebaseapp.com",
+      "messagingSenderId": "654289781621"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage()),
   ],
   providers: [],
   bootstrap: [AppComponent]
